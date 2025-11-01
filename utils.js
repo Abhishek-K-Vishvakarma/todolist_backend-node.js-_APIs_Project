8e92eb309bae1d35b5da1959d580ef91b46819df
Abhishek-K-Vishvakarma <gitrepo101@gmail.com>
2025-11-02 02:30:19 +0530

    Adding SMT for gmail

--- utils.js
+++ utils.js
@@ -1,19 +1,25 @@
 import nodemailer from "nodemailer"
 import dotenv from "dotenv";
 dotenv.config();
-const sendMails = (email, otp)=> {
+const sendMails = (email, otp) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,                // TLS port
+    secure: false,
+    // service: "gmail",
     auth: {
       user: process.env.EMAIL,
       pass: process.env.GMAIL_APP_PASSWORD
-    }
+    },
+    tls: {
+      rejectUnauthorized: false,  // allow self-signed certificates
+    },
   })
   const mailOptions = {
     from: process.env.EMAIL,
     to: email,
     subject: "Your otp for email verification",
-    text : `This otp is : ${otp} for ${email}`
+    text: `This otp is : ${ otp } for ${ email }`
   }
   transporter.sendMail(mailOptions, (err, info) => {
     if (err) {
