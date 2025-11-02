c0aa44879e493e27f2ab97635d83db3941647584
Abhishek-K-Vishvakarma <gitrepo101@gmail.com>
2025-11-03 00:23:02 +0530

    Wrong and split code

--- controllers/controller.js
+++ controllers/controller.js
@@ -20,16 +20,8 @@ import nodeMailer from "nodemailer";
       });
     }
 
-    const existingItem = await AddingSomething.findOne({ name, userId });
-    if (existingItem) {
-      return res.status(400).json({
-        message: "This name already exists in your list!",
-        status: false,
-      });
-    }
     const newItem = new AddingSomething({ name, userId });
     const savedItem = await newItem.save();
-
     res.status(201).json({
       message: "Item added successfully!",
       data: savedItem,
@@ -39,13 +31,6 @@ import nodeMailer from "nodemailer";
     });
 
   } catch (err) {
-    if (err.code === 11000) {
-      return res.status(400).json({
-        message: "Duplicate name for this user!",
-        status: false,
-      });
-    }
-
     res.status(500).json({
       message: "Internal Server Error",
       error: err.message,
